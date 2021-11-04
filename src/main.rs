f2eda117924ba6b6e4b6cabfe576abee99268429
Fabian Wunsch <fabian@uriah.heep.sax.de>
2021-11-04 20:55:58 +0100

    more pretty

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,13 @@ use std::time::Duration;
 use tide::{Response, http::headers::HeaderValues};
 use rand::{thread_rng, RngCore};
 use tide::http::mime;
-use dns_lookup::lookup_addr;
+use async_std_resolver::resolver_from_system_conf;
+use trust_dns_resolver::Name;
+
+struct Config {
+    pub googlebot_redirect: String,
+
+}
 
 #[async_std::main]
 async fn main() -> tide::Result<()>{
@@ -13,12 +19,19 @@ async fn main() -> tide::Result<()>{
     Ok(())
 }
 
-fn is_google(ip_addr: Option<&str>, user_agent: Option<&HeaderValues>) -> bool {
+async fn is_google(ip_addr: Option<&str>, user_agent: Option<&HeaderValues>) -> bool {
     if let (Some(ip_addr), Some(user_agent)) = (ip_addr, user_agent) {
         if user_agent.last().as_str().contains("googlebot") {
             if let Ok(ip) = ip_addr.parse() {
-                if let Ok(host) = lookup_addr(&ip) {
-                    host.ends_with("googlebot.com") || host.ends_with("google.com")
+                let resolver = resolver_from_system_conf().await.unwrap();
+                if let Ok(lookup) = resolver.reverse_lookup(ip).await {
+                    if let Some(host) = lookup.into_iter().next() {
+                        let googlebot = Name::from_utf8("googlebot.com").unwrap();
+                        let google = Name::from_utf8("google.com").unwrap();
+                        googlebot.zone_of(&host) || google.zone_of(&host)
+                    } else {
+                        false
+                    }
                 } else {
                     false
                 }
@@ -34,7 +47,7 @@ fn is_google(ip_addr: Option<&str>, user_agent: Option<&HeaderValues>) -> bool {
 }
 
 async fn handle(request: tide::Request<()>) -> tide::Result {
-    if is_google(request.peer_addr(), request.header("User-Agent")) {
+    if is_google(request.peer_addr(), request.header("User-Agent")).await {
         return Ok(Response::builder(301).header("Location", "https://rpi.heep.sax.de/").build())
     }
     async_std::task::sleep(Duration::from_secs(15)).await;
@@ -45,7 +58,7 @@ async fn handle(request: tide::Request<()>) -> tide::Result {
         std::str::from_utf8_unchecked(&body)
     };
     Ok(Response::builder(200)
-        .body(format!("<!DOCTYPE html><html>{}</html>", body_str))
+        .body(format!("<!DOCTYPE html><html><h1>Wordpress admin page phpinfo pwned you hacked{}</html>", body_str))
         .header("x-powered-by", "PHP/7.3.10")
         .header("server", "Apache/2.4.50 (Unix), mod_killing_mtm")
         .content_type(mime::HTML)
