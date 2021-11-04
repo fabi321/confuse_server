d38cdf61ed7254265f5c296d0752111d8d3b6bd3
Fabian Wunsch <fabian@uriah.heep.sax.de>
2021-11-04 20:27:16 +0100

    added googlebot redirect

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::time::Duration;
-use tide::Response;
+use tide::{Response, http::headers::HeaderValues};
 use rand::{thread_rng, RngCore};
 use tide::http::mime;
+use dns_lookup::lookup_addr;
 
 #[async_std::main]
 async fn main() -> tide::Result<()>{
@@ -12,7 +13,30 @@ async fn main() -> tide::Result<()>{
     Ok(())
 }
 
-async fn handle(_request: tide::Request<()>) -> tide::Result {
+fn is_google(ip_addr: Option<&str>, user_agent: Option<&HeaderValues>) -> bool {
+    if let (Some(ip_addr), Some(user_agent)) = (ip_addr, user_agent) {
+        if user_agent.last().as_str().contains("googlebot") {
+            if let Ok(ip) = ip_addr.parse() {
+                if let Ok(host) = lookup_addr(&ip) {
+                    host.ends_with("googlebot.com") || host.ends_with("google.com")
+                } else {
+                    false
+                }
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    } else {
+        false
+    }
+}
+
+async fn handle(request: tide::Request<()>) -> tide::Result {
+    if is_google(request.peer_addr(), request.header("User-Agent")) {
+        return Ok(Response::builder(301).header("Location", "https://rpi.heep.sax.de/").build())
+    }
     async_std::task::sleep(Duration::from_secs(15)).await;
     let mut rng = thread_rng();
     let mut body = [0u8; 4096];
@@ -23,7 +47,7 @@ async fn handle(_request: tide::Request<()>) -> tide::Result {
     Ok(Response::builder(200)
         .body(format!("<!DOCTYPE html><html>{}</html>", body_str))
         .header("x-powered-by", "PHP/7.3.10")
-        .header("server", "Apache/2.4.50 (Unix)")
+        .header("server", "Apache/2.4.50 (Unix), mod_killing_mtm")
         .content_type(mime::HTML)
         .build())
 }
